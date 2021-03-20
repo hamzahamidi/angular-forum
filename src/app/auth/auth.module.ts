13e272722473dee156f8e68363cff495183a104c
Loic Salou <dev.salou@gmail.com>
2021-03-20 17:07:45 +0100

    installation prettier pour revoir le formatage bien moisi

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,23 +1,14 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {AuthComponent} from './auth.component';
-import {NoAuthGuard} from './no-auth-guard.service';
-import {SharedModule} from '../shared';
-import {AuthRoutingModule} from './auth-routing.module';
-import {TranslateModule} from '@ngx-translate/core';
+import { AuthComponent } from './auth.component';
+import { NoAuthGuard } from './no-auth-guard.service';
+import { SharedModule } from '../shared';
+import { AuthRoutingModule } from './auth-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-            imports: [
-              SharedModule,
-              AuthRoutingModule,
-              TranslateModule
-            ],
-            declarations: [
-              AuthComponent
-            ],
-            providers: [
-              NoAuthGuard
-            ]
-          })
-export class AuthModule {
-}
+  imports: [SharedModule, AuthRoutingModule, TranslateModule],
+  declarations: [AuthComponent],
+  providers: [NoAuthGuard],
+})
+export class AuthModule {}
